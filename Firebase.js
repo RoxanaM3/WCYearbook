ab3ce102648f235ffffb520b8008df9046dc95b4
RoxanaM3 <martinezroxy019@gmail.com>
2025-12-04 23:14:18 -0800

    Add Firebase configuration and initialization
    This file contains the Firebase configuration and initialization code.

---
+++ Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAouO2XczT3EKqebZyTwqj7yFfPeS46csg",
+  authDomain: "yearbook-randomizer.firebaseapp.com",
+  projectId: "yearbook-randomizer",
+  storageBucket: "yearbook-randomizer.firebasestorage.app",
+  messagingSenderId: "127922673093",
+  appId: "1:127922673093:web:67c4286b84a062f35827ac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
